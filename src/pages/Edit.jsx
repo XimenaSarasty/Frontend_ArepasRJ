ff19427673bf486d7a6163091e7665dbd4292b08
Miguel <miguelescobarp03@gmail.com>
2024-03-17 23:38:39 -0500

    pequeño arreglo

---
+++ src/pages/Edit.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import EditProduct from '../components/EditProduct'
+import NavAdm from '../components/NavAdm'
+
+const Edit = () => {
+  return (
+    <div>
+        <NavAdm />
+      <EditProduct />
+    </div>
+  )
+}
+
+export default Edit
